bbb9f99d9a8c7d5db5f8a62abae0c05c42cab4d5
MajoCabre <cabremariajose@gmail.com>
2021-10-26 17:17:16 -0300

    Agregando botones contadores

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./Footer";
 import ItemListContainer from "./Components/ItemListContainer";
+import ItemCount from "./Components/ItemCount";
 
 const App = () => {
 	return (
 		<>
 		<NavBar/>
 		<ItemListContainer greeting="mundo"/>
+		<ItemCount stock="5"/>     
 		<Footer/>
 		</>
 		)
---
+++ src/Components/ItemCount.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const ItemCount = () => {
+
+    const [stock, setContador] = useState(0)
+
+    const stockContador = () =>{
+
+        if (stock< 5){
+            setContador(stock +1)
+        }
+        else {
+            console.log("error")
+        }
+    }
+
+    const restaContador = () => {
+
+        if(stock > 0){
+            setContador(stock -1)
+        } else {
+            console.log("error")
+        }
+    }
+
+
+    return (
+        <>
+        <nav className="button">
+        <button className="button" onClick={stockContador}>
+            <span className="material-icons"> add_circle_outline </span>
+        </button>
+
+        <button className="button" onClick={restaContador}>
+            <span className="material-icons">remove_circle_outline</span>        
+        </button>
+        </nav>
+
+        <nav className="button">
+            <p className="button">{stock}</p>
+        </nav>
+
+
+
+
+
+
+        </>
+    )
+}
+
+export default ItemCount;
